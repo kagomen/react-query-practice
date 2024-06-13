70923af0411e8ea06b59dd9c3477d9092a2f9712
kagome <fu.taz.1102@gmail.com>
2024-06-14 02:28:12 +0900

    元に戻す

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,15 +6,13 @@ import Loading from './Loading'
 
 function App() {
   const [books, setBooks] = useState(null)
-  // const url = 'https://rakuten-api-proxy-practice-backend.kagome.workers.dev'
+  const url = 'https://rakuten-api-proxy-practice-backend.kagome.workers.dev'
 
   async function search(keyword) {
     try {
-      // const res = await axios.get(`${url}/search/${keyword}`)
-      const res = await fetch(`/search/${keyword}`)
-      const data = await res.json();
-      setBooks(data.Items)
-      console.log('search', data.Items)
+      const res = await axios.get(`${url}/search/${keyword}`)
+      setBooks(res.data.Items)
+      console.log('search', res.data.Items)
     } catch (error) {
       console.error('Error: ', error)
     }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,12 +6,12 @@ export default defineConfig({
   plugins: [react()],
   // 以下、本番環境でうまく動作しないため要調査
   // App.jsxにurlを直書きすることで問題なく動作した
-  server: {
-    proxy: {
-      '/search': {
-        target: 'https://rakuten-api-proxy-practice-backend.kagome.workers.dev',
-        changeOrigin: true,
-      }
-    }
-  }
+  // server: {
+  //   proxy: {
+  //     '/search': {
+  //       target: 'https://rakuten-api-proxy-practice-backend.kagome.workers.dev',
+  //       changeOrigin: true,
+  //     }
+  //   }
+  // }
 })
