226e1561f0ccdced58ab19f282b2b89210972367
kagome <fu.taz.1102@gmail.com>
2024-06-14 12:33:10 +0900

    test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,21 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 以下、本番環境でうまく動作しないため要調査
-  // App.jsxにurlを直書きすることで問題なく動作した
-  // server: {
-  //   proxy: {
-  //     '/search': {
-  //       target: 'https://rakuten-api-proxy-practice-backend.kagome.workers.dev',
-  //       changeOrigin: true,
-  //     }
-  //   }
-  // }
   server: {
-    port: 5173, // 必要に応じてデフォルトのポートを設定
     proxy: {
       '/search': {
-        target: 'https://rakuten-api-proxy-practice-backend.kagome.workers.dev', // Honoサーバーのアドレス
+        target: 'https://rakuten-api-proxy-practice-backend.kagome.workers.dev',
         changeOrigin: true
       }
     }
