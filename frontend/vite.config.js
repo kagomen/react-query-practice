1d6ea00ec0c494e1d599a655e6e42390df43e2de
kagome <fu.taz.1102@gmail.com>
2024-06-14 12:55:13 +0900

    test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,14 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-
-  const env = loadEnv(mode, process.cwd(), '')
-
-  return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/search': {
-          target: env.VITE_SERVER_URL_FOR_CONFIG,
-          changeOrigin: true
-        }
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/search': {
+        target: 'https://rakuten-api-proxy-practice-backend.kagome.workers.dev',
+        changeOrigin: true
       }
     }
   }
