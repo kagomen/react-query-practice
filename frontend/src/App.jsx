e80f854f8d13415931974272a5302698a541e614
kagome <fu.taz.1102@gmail.com>
2024-06-15 14:32:04 +0900

    全データ読み込んだ際のメッセージを表示させる

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Loading from './Loading'
 // import useSWR from 'swr'
 import useSWRInfinite from 'swr/infinite'
 import MoreLoadBtn from './MoreLoadBtn'
+import ReachingEndMessage from './ReachingEndMessage'
 
 async function fetcher(url) {
   const res = await axios.get(url)
@@ -14,6 +15,7 @@ async function fetcher(url) {
 
 function App() {
   const [keyword, setKeyword] = useState('React')
+  const [pageCount, setPageCount] = useState(null)
 
   // const url = `${import.meta.env.VITE_SERVER_URL}/search/${keyword}`
   // const { data, error, isLoading, mutate } = useSWR(url, fetcher)
@@ -21,13 +23,22 @@ function App() {
   const getKey = (pageIndex, previousPageData) => {
     console.log('pageIndex:', pageIndex)
     console.log('previousPageData:', previousPageData)
-    if (previousPageData && !previousPageData.Items.length) return null // 最後のページに到達した場合
+    if (previousPageData && !previousPageData.Items.length) return null
     return `${import.meta.env.VITE_SERVER_URL}/search/${keyword}/${pageIndex + 1}`
   }
 
   const { data, error, size, setSize, isValidating, isLoading } = useSWRInfinite(getKey, fetcher, {
-    revalidateOnFocus: false // フォーカスが戻ったときのリフェッチを無効にする
+    revalidateOnFocus: false, // フォーカスが戻ったときのリフェッチを無効にする
+    onSuccess: (data) => {
+      // ページ総数を取得
+      if (data && pageCount === null) {  // 初回フェッチ時のみ実行
+        console.log('ページ総数を取得')
+        setPageCount(data[0]?.pageCount)
+      }
+    }
   })
+
+  const isReachingEnd = pageCount !== null && size >= pageCount
 
   // useSWRInfiniteが返すdataは、通常のres.dataで受け取る形式とは異なるため、以下の作業が必要
   // 理由：ページ更新ごとにフェッチする→ページ更新ごとにオブジェクトに格納してる
@@ -50,6 +61,7 @@ function App() {
 
   function search(newKeyword) {
     setKeyword(newKeyword)
+    setPageCount(null)
   }
 
   if (error) {
@@ -64,7 +76,11 @@ function App() {
     <div className='my-8'>
       <SearchBar search={search} />
       {books.length > 0 && <BookList books={books} />}
-      {isValidating ? <Loading /> : <MoreLoadBtn loadMore={loadMore} />}
+      {isValidating ? (
+        <Loading />
+      ) : (
+        isReachingEnd ? <ReachingEndMessage /> : <MoreLoadBtn loadMore={loadMore} />
+      )}
     </div>
   )
 }
---
+++ frontend/src/ReachingEndMessage.jsx
@@ -0,0 +1,7 @@
+const ReachingEndMessage = () => {
+  return (
+    <div className="w-fit mx-auto">すべてのアイテムを表示しました</div>
+  )
+}
+
+export default ReachingEndMessage
